14ffcad844fd140814aa22d8d9545b643208fa3d
Tyuiu.RyabtsevNE@outlook.com <github_090506@mail.ru>
2024-11-11 21:39:21 +0500

    Task0

---
+++ Tyuiu.RyabtsevNE.Sprint4.Task0.V16.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.RyabtsevNE.Sprint4.Task0.V16.Lib;
+
+namespace Tyuiu.RyabtsevNE.Sprint4.Task0.V16.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestClass]
+        public class DataServiceTest
+        {
+            [TestMethod]
+            public void TestMethod1()
+            {
+                DataService ds = new DataService();
+
+                int[] numsArray = { 2, 6, 2, 3, 4, 5, 4, 9, 7, 8 };
+                int res = ds.GetMultEvenArrEl(numsArray);
+                int Wait = 3072;
+
+                Assert.AreEqual(Wait, res);
+            }
+    }
+}
