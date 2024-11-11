6666b688938ab7896512c2d7a2538e0967e3b707
Tyuiu.RyabtsevNE@outlook.com <github_090506@mail.ru>
2024-11-11 20:41:55 +0500

    Task3

---
+++ Tyuiu.RyabtsevNE.Sprint4.Task3.V30.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.RyabtsevNE.Sprint4.Task3.V30.Lib
+{
+    public class DataService : ISprint4Task3V30
+    {
+        public int Calculate(int[,] array)
+        {
+            int max = 0;
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                if (array[2, i] > max)
+                    max = array[2, i];
+            }
+            return max;
+        }
+    }
+}
