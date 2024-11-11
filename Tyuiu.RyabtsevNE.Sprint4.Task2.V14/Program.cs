2d3ee7d52668fcc298118df93ff8a1afd412fcef
Tyuiu.RyabtsevNE@outlook.com <github_090506@mail.ru>
2024-11-11 21:45:42 +0500

    Task2

---
+++ Tyuiu.RyabtsevNE.Sprint4.Task2.V14/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.RyabtsevNE.Sprint4.Task2.V14.Lib;
+
+namespace Tyuiu.RyabtsevNE.Sprint4.Task2.V14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнил: Рябцев Н. Е. | СМАРТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Одномерные массивы. (генератор случайных чисел)                   *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #14                                                             *");
+            Console.WriteLine("* Выполнил: Рябцев Никита Евгеньевич | СМАРТб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 12 элементов заполненный         *");
+            Console.WriteLine("* случайными в диапазоне от 1 до 8 подсчитать произведение нечетных       *");
+            Console.WriteLine("* элементов массива.                                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[] array = new int[12];
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                array[i] = rnd.Next(1, 9);
+            }
+            Console.WriteLine("Массив:");
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                Console.Write(array[i] + "\t");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            var result = ds.Calculate(array);
+            Console.WriteLine("Произведение = " + result);
+            Console.ReadKey();
+        }
+    }
+}
